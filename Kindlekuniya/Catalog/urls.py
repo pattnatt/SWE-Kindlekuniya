a5827c9c6697562f4b7428a68ae6225ccb5e7d55
Pattnatt <jittapat007@gmail.com>
2017-09-28 20:24:34 +0700

    create base html & product's detail page

--- Kindlekuniya/Catalog/urls.py
+++ Kindlekuniya/Catalog/urls.py
@@ -2,8 +2,13 @@ from django.conf.urls import url, include
 from django.contrib.sites.models import Site
 from . import views
 
+app_name = 'Catalog'
+
 urlpatterns = [
     # /catalog/ -> catalog homepage
     url(r'^$', views.index, name = 'index'),
+    # /catalog/search -> catalog search
     url(r'^search/', include('haystack.urls')),
+    # /catalog/details/(number) -> product's details
+    url(r'^detail/(?P<product_id>[0-9]+)/$', views.detail, name = 'detail'),
 ]
--- Kindlekuniya/Catalog/views.py
+++ Kindlekuniya/Catalog/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
+from .models import Product
 
 def index(request):
     context = {
     }
     return render(request, 'index.html', context)
+
+def detail(request, product_id):
+    product = Product.objects.get(id = int(product_id))
+    context = {
+        'product' : product
+    }
+    return render(request, 'detail.html', context)
