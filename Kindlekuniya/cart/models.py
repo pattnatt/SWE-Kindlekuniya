a34ded63f676314c68a7259a0c8948910cd52e9e
peak <nitidhero@outlook.com>
2017-09-28 21:25:47 +0700

    merge stock to product

--- Kindlekuniya/cart/models.py
+++ Kindlekuniya/cart/models.py
@@ -2,28 +2,24 @@ from django.db import models
 
 class Product(models.Model):
 
+    quantity = models.IntegerField(default=0)
     isbn = models.CharField(max_length=30)
     name = models.CharField(max_length=30)
     author = models.CharField(max_length=30)
     publisher = models.CharField(max_length=30)
     price = models.IntegerField()
     weight = models.IntegerField()
-    isMonocrome = models.BinaryField()
+    isMonocrome = models.BooleanField()
     paperType = models.CharField(max_length=30)
     coverType = models.CharField(max_length=30)
     page = models.IntegerField()
     size = models.CharField(max_length=30)
-    discription = models.CharField(max_length=240)
+    discription = models.TextField()
     pictureUrl = models.URLField()
-
+    
     def __str__(self):
         return self.name
 
 
-class Stock(models.Model):
-
-    quantity = models.IntegerField()
-    product = models.OneToOneField(Product)
-    
         
         
