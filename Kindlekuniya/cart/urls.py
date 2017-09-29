5152e8b07996614291c0086f3c63175828bea3d1
peak <nitidhero@outlook.com>
2017-09-29 17:27:25 +0700

    better cart page

--- Kindlekuniya/cart/urls.py
+++ Kindlekuniya/cart/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.cart, name='cart'),
+    url(r'^$', views.IndexView.as_view(), name='cart'),
 ]
--- Kindlekuniya/cart/views.py
+++ Kindlekuniya/cart/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
 
 # Create your views here.
 
 from .models import Product
 
-def cart(request):
-    cartItem = Product.objects.filter()
-    return render(request ,'cart/cart.html',{'cartItem':cartItem})
+class IndexView(generic.ListView):
+    template_name = 'cart/cart.html'
+    context_object_name = 'cartItem_list'
+
+    def get_queryset(self):
+       
+        return Product.objects.filter()
+
+
+
