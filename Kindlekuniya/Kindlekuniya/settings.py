0a334550230a2fd362a8f0159a52af5c46eeb29b
popobaza <nitidhero@outlook.com>
2017-10-24 11:04:39 +0700

    update docker

--- Kindlekuniya/Kindlekuniya/settings.py
+++ Kindlekuniya/Kindlekuniya/settings.py
@@ -30,7 +30,10 @@ SECRET_KEY = 'n(=p(9uru3qhm(290sfl@i41co40zt@k($7r1$p9*ku=d)*1*r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '0.0.0.0',
+    'localhost',
+]
 # Application definition
 
 INSTALLED_APPS = [
@@ -54,6 +57,7 @@ INSTALLED_APPS = [
     'user',
     'bootstrapform',
     'cart.apps.CartConfig',
+    'passlib',
 ]
 
 SITE_ID = 1
