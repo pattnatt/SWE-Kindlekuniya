3391ef1a33fcdc3fe30841eea67324c23f9adde7
skconan <supakit.kr@gmail.com>
2017-10-30 00:54:00 -0700

    setting email

--- Kindlekuniya/Kindlekuniya/settings.py
+++ Kindlekuniya/Kindlekuniya/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 EMAIL_HOST =  'smtp.gmail.com'
 EMAIL_HOST_USER = 'supakit.kr@gmail.com'
-EMAIL_HOST_PASSWORD = 'sk@Gkudo2518'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
 
