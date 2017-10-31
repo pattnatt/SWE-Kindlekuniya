c654691417d1604f79fedabaf075627d5517fc0a
skconan <supakit.kr@gmail.com>
2017-10-31 06:48:53 -0700

    change integer id to uuid and add FK to addr table

--- Kindlekuniya/user/models.py
+++ Kindlekuniya/user/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.forms import ModelForm
 from passlib.hash import pbkdf2_sha256
+import uuid
 
 
 class User(models.Model):
@@ -9,39 +10,48 @@ class User(models.Model):
         ('CL', 'Closed'),
         ('WT', 'Waiting'),
     )
-
-    user_id = models.AutoField(primary_key=True, default=None)
+    
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        unique=True
+    )
     email = models.EmailField(max_length=120, default=None, unique=True)
     firstname = models.CharField(max_length=120, default=None)
     lastname = models.CharField(max_length=120, default=None)
     is_activated = models.CharField(
         max_length=2,
         choices=ACTIVE_TYPE,
-        default='WT',
+        default='WT'
     )
-    token = models.CharField(max_length=120, default='0')
+    forget_password = models.BooleanField(default=False)
     password = models.CharField(max_length=512, default=None)
     phone_number = models.CharField(max_length=10, default=None)
 
     def __str__(self):
         return self.email
 
-
 class Address(models.Model):
-    addr_id = models.AutoField(primary_key=True, default=None)
-    user_id = models.IntegerField(default=None)
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        unique=True
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     addr = models.CharField(max_length=512, default=None)
     city = models.CharField(max_length=128, default=None)
-    zip = models.CharField(max_length=5, default=None)
+    zipcode = models.CharField(max_length=5, default=None)
 
     def __str__(self):
-        return self.addr + " " + self.city + " " + self.zip
+        return self.user.email
 
 
 class AddressModelForm(ModelForm):
     class Meta:
         model = Address
-        fields = ['addr', 'city', 'zip']
+        fields = ['addr', 'city', 'zipcode','user']
 
 
 class SignupModelForm(ModelForm):
