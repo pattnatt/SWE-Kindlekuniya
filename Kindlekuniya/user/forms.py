89445c4c4949f65a227afa55399b2a7a9b3667aa
skconan <supakit.kr@gmail.com>
2017-10-30 22:40:28 -0700

    created forms for edit_profile change_password reset_password

--- Kindlekuniya/user/forms.py
+++ Kindlekuniya/user/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 from django.forms import ModelForm
 from django.core.validators import RegexValidator
-from .models import SignupModelForm, User
-
+from .models import SignupModelForm, User, Address
+from passlib.hash import pbkdf2_sha256
 
 class SignupForm(forms.Form):
     email = forms.EmailField(required=True, max_length=128)
@@ -19,7 +19,7 @@ class SignupForm(forms.Form):
         )]
     )
     confirm_password = forms.CharField(
-        required=True, max_length=128, widget=forms.PasswordInput)
+        required=True, widget=forms.PasswordInput)
     phone_number = forms.CharField(
         required=True,
         max_length=10,
@@ -80,14 +80,33 @@ class SigninForm(forms.Form):
         return self.cleaned_data
 
 class EditProfileForm(forms.Form):
+    email = forms.EmailField(widget=forms.TextInput(attrs={'readonly':'readonly'}))      
     firstname = forms.CharField(required=True, max_length=128)
     lastname = forms.CharField(required=True, max_length=128)
+    phone_number = forms.CharField(
+        required=True,
+        max_length=10,
+        min_length=10,
+        validators=[RegexValidator(
+            regex='^[0][0-9]*$',
+            message='Phone number must be numeric 0-9 and Please lengthen phone number to 10.'
+        ), ]
+    )
+
+class EmailForm(forms.Form):
+    email = forms.EmailField(required=True, max_length=128)    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if not User.objects.filter(email=email):
+            raise forms.ValidationError("Email does not exist.")
+        return self.cleaned_data
+
+class ChangePasswordForm(forms.Form):
+    email = forms.EmailField(widget=forms.TextInput(attrs={'readonly':'readonly'}))          
     old_password = forms.CharField(
         required=True,
-        min_length=8,
-        max_length=128,
         widget=forms.PasswordInput
-    )
+     )
     new_password = forms.CharField(
         required=True,
         min_length=8,
@@ -99,25 +118,71 @@ class EditProfileForm(forms.Form):
         )]
     )
     confirm_password = forms.CharField(
-        required=True, max_length=128, widget=forms.PasswordInput)
-    phone_number = forms.CharField(
+        required=True, widget=forms.PasswordInput)
+    
+
+    def clean_confirm_password(self):
+        password1 = self.cleaned_data.get('new_password')
+        password2 = self.cleaned_data.get('confirm_password')
+
+        if password1 and password1 != password2:
+            raise forms.ValidationError("New password don't match.")
+
+        return self.cleaned_data
+
+class ForgotPasswordForm(forms.Form):
+    new_password = forms.CharField(
         required=True,
-        max_length=10,
-        min_length=10,
+        min_length=8,
+        max_length=128,
+        widget=forms.PasswordInput,
         validators=[RegexValidator(
-            regex='^[0][0-9]*$',
-            message='Phone number must be numeric 0-9 and Please lengthen phone number to 10.'
-        ), ]
+            regex='^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$',
+            message='Password must contain at least one alphabet and one digit'
+        )]
     )
-    address = forms.CharField(
-        required=True, max_length=128, widget=forms.Textarea())
-    city = forms.CharField(required=True, max_length=128)
-    zipcode = forms.CharField(
-        required=True,
-        max_length=5,
-        min_length=5,
-        validators=[RegexValidator(
-            regex='^[0-9]*$',
-            message='Zip code must be numeric 0-9 and Please lengthen phone number to 5.'
-        ), ]
-    )
+    confirm_password = forms.CharField(
+        required=True, widget=forms.PasswordInput)
+    
+
+    def clean_confirm_password(self):
+        password1 = self.cleaned_data.get('new_password')
+        password2 = self.cleaned_data.get('confirm_password')
+
+        if password1 and password1 != password2:
+            raise forms.ValidationError("Password don't match.")
+
+        return self.cleaned_data
+
+                
+# class EditAddress(forms.Form):
+#         # self.fields['address'] = forms.CharField(
+#         #     required=True, 
+#         #     max_length=128, 
+#         #     widget=forms.Textarea(),
+#         #     initial=user.address
+#         # )
+#         # self.fields['city'] = forms.CharField(
+#         #     required=True, 
+#         #     max_length=128,
+#         #     initial=user.city
+#         # )
+#         # self.fields['zipcode'] = forms.CharField(
+#         #     required=True,
+#         #     max_length=5,
+#         #     min_length=5,
+#         #     validators=[RegexValidator(
+#         #         regex='^[0-9]*$',
+#         #         message='Zip code must be numeric 0-9 and Please lengthen phone number to 5.'
+#         #     ), ],
+#         #     initial=user.zipcode
+#         # )
+#     def a():
+#         pass
+    
+    # address = forms.CharField()
+    # city = forms.CharField()
+    # zipcode = forms.CharField()
+    
+    
+    
